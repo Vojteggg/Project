9cd8fa8695c140d7b5184ce457eb721f4dc83f30
vojteggg <wlos@st.swps.edu.pl>
2022-04-05 15:16:15 +0200

    wywal sie jeszcze raz to wyjebe v2

--- src/main/java/TrojkatCreator.java
+++ src/main/java/TrojkatCreator.java
@@ -3,11 +3,18 @@ public class TrojkatCreator {
     Punkt b;
     Punkt c;
     TrojkatA s = new TrojkatA(a,b,c);
+    Punkt x;
+    Punkt y;
+    Punkt z;
 
-    public TrojkatCreator(Punkt a, Punkt b, Punkt c){
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    public TrojkatCreator(Punkt x, Punkt y, Punkt z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public czyRowne (){
+        if (x.x==a.x && x.y==a.y)
     }
 
 
